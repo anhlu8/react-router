17a82b1e080788cc52941d27f4bd8b5ea29eb67b
Anh Lu <anhlucci@outlook.com>
2018-09-29 13:16:35 -0400

    servers.js fixed

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
